f194c1747f25d9ef3f3a312f44c8d213d130cb0a
austinkelsay <austinkelsay@yahoo.com>
2020-08-28 18:30:26 -0500

    RB test running

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = [None] * capacity
+        self.tail = -1
+        self.head = 0
+        self.size = 0
 
     def append(self, item):
-        pass
+        if self.size == self.capacity:
+            print('RB is full')
+            return None
+
+        else:
+            self.tail = (self.tail + 1) % self.capacity
+            self.data[self.tail] = item
+            self.size += 1
 
     def get(self):
-        pass
+        return self.data
