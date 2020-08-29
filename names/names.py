e18e800c0120a08830e617ca699e45f23e80f03f
austinkelsay <austinkelsay@yahoo.com>
2020-08-28 19:35:16 -0500

    Finished names with stretch!

--- names/names.py
+++ names/names.py
@@ -1,22 +1,26 @@
 import time
+import numpy
 
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
+arr1 = numpy.array(names_1)
 f.close()
 
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
+arr2 = numpy.array(names_2)
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+duplicates = [] # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+def compare(a,b):
+    c = [value for value in a if value in b]
+    return c
+
+duplicates = compare(arr1,arr2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
